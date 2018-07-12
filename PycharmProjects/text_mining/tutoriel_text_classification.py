92abf44ced55bb0e9a68a9f42b2d1aaccec23a51
faolin <f.danselme@gmail.com>
2018-07-12 16:28:19 +0200

    ajout de la possibilité de pouvoir tester le modéle entrainé sur une phrase

--- PycharmProjects/text_mining/tutoriel_text_classification.py
+++ PycharmProjects/text_mining/tutoriel_text_classification.py
@@ -30,19 +30,21 @@ def tfidf_process(data):
     transformer = transformer.fit(data)
     return transformer
 
+SOURCE = "/home/francois/Téléchargements/imdb.csv"
+ENCODING = "ISO-8859-1"
 
+#SOURCE = "pvcp.csv"
+#ENCODING = "UTF-8"
 
-#SOURCE = "imdb.csv"
-#ENCODING = "ISO-8859-1"
-
-SOURCE = "pvcp.csv"
-ENCODING = "UTF-8"
 def retrieve_data(source_name=SOURCE):
     import pandas as pd
     import numpy as np
     frac = 0.9
 
     df = pd.read_csv(source_name, header=0, encoding=ENCODING)
+    df = df[df.text.notnull()]
+    print(df.text == '')
+
     df['split'] = np.random.randn(df.shape[0], 1)
 
     msk = np.random.rand(len(df)) <= frac
@@ -84,25 +86,54 @@ def train_and_test(vectorizer, training_function):
     print ("Train accuracy: ", accuracy(Ytrain, Ytrain_uni))
     print ("\n")
 
-    print ("-----------------------ANALYSIS ON THE TEST DATA ---------------------------")
+    print ("-----------------------TESTING THE MODEL -----------------------")
     Xtest_uni = vectorizer.transform(Xtest_text)
     Ytest_uni = classifier.predict(Xtest_uni)
     print ("Test accuracy: ", accuracy(Ytest, Ytest_uni))
     print ("\n")
-    return(Ytest_uni)
+    return Ytest_uni, classifier
+
 
 
 if __name__ == "__main__":
     import time
     from sklearn.metrics import confusion_matrix
-    Xtrain_text, Ytrain, Xtest_text, Ytest = retrieve_data()
-    print("========================UNIGRAM + SGD================================")
-    start = time.time()
-    y_true=train_and_test(unigram_process(Xtrain_text), train_sgd)
-    #y_true_bigram=train_and_test(bigram_process(Xtrain_text), train_sgd)
-    print ("Time taken: ", time.time() - start, " seconds")
-    print ("test",Ytest)
-    Matrice_confusion=confusion_matrix(Ytest,y_true)
-    print("matrice_confusion_unigram",Matrice_confusion)
-    #Matrice_confusion_bigram=confusion_matrix(Ytest,y_true_bigram)
-    #print("matrice_confusion_bigram",Matrice_confusion_bigram)
+    import argparse
+    from sklearn.externals import joblib
+
+    MODEL_PATH = 'model.pkl'
+
+    parser = argparse.ArgumentParser(description='Train and test a semantic classifier.')
+    parser.add_argument('action', metavar='action', type=str, nargs='+',
+                        help='train | infer')
+
+    args = parser.parse_args()
+
+    action = args.action[0]
+
+
+    if action == 'train':
+        Xtrain_text, Ytrain, Xtest_text, Ytest = retrieve_data()
+        print("========================UNIGRAM + SGD================================")
+        start = time.time()
+        vectorizer = bigram_process(Xtrain_text)
+        y_true, classifier=train_and_test(vectorizer, train_sgd)
+        #y_true_bigram=train_and_test(bigram_process(Xtrain_text), train_sgd)
+        print ("Time taken: ", time.time() - start, " seconds")
+        print ("test",Ytest)
+        Matrice_confusion=confusion_matrix(Ytest,y_true)
+        print("matrice_confusion_unigram",Matrice_confusion)
+        #Matrice_confusion_bigram=confusion_matrix(Ytest,y_true_bigram)
+        #print("matrice_confusion_bigram",Matrice_confusion_bigram)
+
+        print ("-----------------------SAVING THE MODEL-----------------------------")
+        joblib.dump(classifier, MODEL_PATH)
+        joblib.dump(vectorizer, 'vectorizer.pkl')
+        print("Model saved.")
+    else:
+        model = joblib.load(MODEL_PATH)
+        vectorizer = joblib.load('vectorizer.pkl')
+        test_text = "this is a terrible movie, i hate it"
+        test_vector = vectorizer.transform([test_text])
+        prediction = model.predict(test_vector)
+        print("prediction", prediction)
