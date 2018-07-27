d1c2f89bb7ece2b2f1616dc0f57b48395fbd17da
faolin <f.danselme@gmail.com>
2018-07-27 16:21:36 +0200

    entraine un modele SVM avec bigram ou unigram + tfidf et le test

--- PycharmProjects/text_mining/twitter_classification_train.py
+++ PycharmProjects/text_mining/twitter_classification_train.py
@@ -1,7 +1,10 @@
+'''entraine un modele SVM avec bigram ou unigram + tfidf et le test'''
 
-
-
-
+def remove_stopwords(sentence, stopwords):
+    sentencewords = sentence.split()
+    resultwords = [word for word in sentencewords if word.lower() not in stopwords]
+    result = ' '.join(resultwords)
+    return result
 
 
 def unigram_process(data):
@@ -29,39 +32,46 @@ def tfidf_process(data):
     transformer = transformer.fit(data)
     return transformer
 
+SOURCE = "/home/francois/Téléchargements/imdb.csv"
+ENCODING = "ISO-8859-1"
 
+#SOURCE = "pvcp.csv"
+#ENCODING = "UTF-8"
 
-def retrieve_data():
-    import json
-    import urllib.request
-    import numpy as np
+def retrieve_data(source_name=SOURCE):
     import pandas as pd
-    frac = 0.8
-    #infile_name ='/home/francois/Téléchargements/tweet_base_train.json'
+    import numpy as np
+    import json
+    frac = 0.9
+
+    infile_name = 'Base_tweets_finale.json'
     all_tweets = []
     all_notation = []
 
-    #with open(infile_name, 'r') as infile :
-    contents = urllib.request.urlopen("https://mds-dev.sinay.fr/api/tweet").read()
-    contents = contents.decode('utf-8')
-    json_load = json.loads(contents)
-    '''raw_text = infile.read()
-    json_load = json.loads(raw_text, 'utf-8')'''
+    with open(infile_name, 'r') as infile:
+        '''contents = urllib.request.urlopen("https://mds-dev.sinay.fr/api/tweet").read()
+        contents = contents.decode('utf-8')
+        json_load = json.loads(contents)
+        '''
+        raw_text = infile.read()
+        json_load = json.loads(raw_text, 'utf-8')
 
-    for i in range(len(json_load)):
-        if json_load[i]['interessant'] != None:
-            all_tweets.append(json_load[i]['nom_nettoye'])
-            all_notation.append(json_load[i]['interessant'])
+        for i in range(len(json_load)):
+            if json_load[i]['interessant'] != None:
+                if json_load[i]['nom_nettoye'] != None:
+                    all_tweets.append(json_load[i]['nom_nettoye'])
+                    all_notation.append(json_load[i]['interessant'])
 
-    df = pd.DataFrame({'tweet': all_tweets, 'interessant': all_notation})
+        df = pd.DataFrame({'tweet': all_tweets, 'interessant': all_notation})
+        df['split'] = np.random.randn(df.shape[0], 1)
+        msk = np.random.rand(len(df)) <= frac
+        train = df[msk]
+        print(train)
 
-    df['split'] = np.random.randn(df.shape[0], 1)
-    msk = np.random.rand(len(df)) <= frac
-    train = df[msk]
-    print(train)
-    test = df[~msk]
-    print('\n', test)
+        test = df[~msk]
+
     return train['tweet'], train['interessant'], test['tweet'], test['interessant']
+
 
 
 def train_sgd(Xtrain, Ytrain):
@@ -88,32 +98,71 @@ def write_txt(data, name):
 
 def train_and_test(vectorizer, training_function):
     print ("-----------------------TRAINING THE MODEL---------------------------")
+    # avec TF-IDF
+    '''Xtrain_uni = vectorizer.transform(Xtrain_text)
+    uni_tfidf_transformer = tfidf_process(Xtrain_uni)
+    Xtrain_tf_uni = uni_tfidf_transformer.transform(Xtrain_uni)
+    classifier = training_function(Xtrain_tf_uni, Ytrain)
+    Ytrain_uni = classifier.predict(Xtrain_tf_uni)'''
+
+    # sans TF-IDF
     Xtrain_uni = vectorizer.transform(Xtrain_text)
     classifier = training_function(Xtrain_uni, Ytrain)
     Ytrain_uni = classifier.predict(Xtrain_uni)
+
+
+
     print ("Train accuracy: ", accuracy(Ytrain, Ytrain_uni))
     print ("\n")
 
-    print ("-----------------------ANALYSIS ON THE TEST DATA ---------------------------")
+    print ("-----------------------TESTING THE MODEL -----------------------")
     Xtest_uni = vectorizer.transform(Xtest_text)
     Ytest_uni = classifier.predict(Xtest_uni)
     print ("Test accuracy: ", accuracy(Ytest, Ytest_uni))
     print ("\n")
-    return(Ytest_uni)
+    return Ytest_uni, classifier
+
 
 
 if __name__ == "__main__":
     import time
     from sklearn.metrics import confusion_matrix
-    Xtrain_text, Ytrain, Xtest_text, Ytest = retrieve_data()
-    print("========================UNIGRAM + SGD================================")
-    start = time.time()
-    y_true=train_and_test(unigram_process(Xtrain_text), train_sgd)
-    print("y_true: ", y_true)
-    #y_true_bigram=train_and_test(bigram_process(Xtrain_text), train_sgd)
-    print ("Time taken: ", time.time() - start, " seconds")
-    print ("test",Ytest)
-    Matrice_confusion=confusion_matrix(Ytest,y_true)
-    print("matrice_confusion_unigram",Matrice_confusion)
-    #Matrice_confusion_bigram=confusion_matrix(Ytest,y_true_bigram)
-    #print("matrice_confusion_bigram",Matrice_confusion_bigram)
+    import argparse
+    from sklearn.externals import joblib
+
+    MODEL_PATH = 'model.pkl'
+
+    parser = argparse.ArgumentParser(description='Train and test a semantic classifier.')
+    parser.add_argument('action', metavar='action', type=str, nargs='+',
+                        help='train | infer')
+
+    args = parser.parse_args()
+
+    action = args.action[0]
+
+
+    if action == 'train':
+        Xtrain_text, Ytrain, Xtest_text, Ytest = retrieve_data()
+        print("========================BIGRAM + SGD================================")
+        start = time.time()
+        vectorizer = bigram_process(Xtrain_text) # on chois bigrame ou unigram ici
+        y_true, classifier=train_and_test(vectorizer, train_sgd)
+        #y_true_bigram=train_and_test(bigram_process(Xtrain_text), train_sgd)
+        print ("Time taken: ", time.time() - start, " seconds")
+        print ("test",Ytest)
+        Matrice_confusion=confusion_matrix(Ytest,y_true)
+        print("matrice_confusion_unigram",Matrice_confusion)
+        #Matrice_confusion_bigram=confusion_matrix(Ytest,y_true_bigram)
+        #print("matrice_confusion_bigram",Matrice_confusion_bigram)
+
+        print ("-----------------------SAVING THE MODEL-----------------------------")
+        joblib.dump(classifier, MODEL_PATH)
+        joblib.dump(vectorizer, 'vectorizer.pkl')
+        print("Model saved.")
+    else:
+        model = joblib.load(MODEL_PATH)
+        vectorizer = joblib.load('vectorizer.pkl')
+        test_text = "RT @DukeU: A strategy for making 'no-mining zones' in the deep sea. https://t.co/EQpWNMsS62 https://t.co/vaRRZMx7MJ"
+        test_vector = vectorizer.transform([test_text])
+        prediction = model.predict(test_vector)
+        print("prediction", prediction)
